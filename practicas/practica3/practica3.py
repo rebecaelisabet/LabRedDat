693cd2b85ff8256f79b3663cb1d1e4dc3cd4105e
rebeca <elabarrera3@gmail.com>
2024-04-27 21:47:01 -0600

    Corriguendo el código

--- practicas/practica3/practica3.py
+++ practicas/practica3/practica3.py
@@ -1,10 +1,6 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-from scipy import stats as sst
-from scipy import optimize as sco
-import math
-import plotly.express as px
 import matplotlib.pyplot as plt
 import plotly.graph_objects as go
  
